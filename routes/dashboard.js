495c8c00c336d911a7cb222ea698ee0deb6c1916
John Pribesh <beopensourcellc@gmail.com>
2014-12-05 02:01:47 -0500

    Changed dashboard route to use db

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,39 +1,20 @@
 var express = require('express');
-var router = express.Router();
-var stormpath = require('express-stormpath');
+var router = express.Router()
 var _ = require('lodash');
-var Firebase = require('firebase');
 var inspect = require('util');
-var Client = require('mariasql');
-var c = new Client();
+var Sequelize = require('sequelize');
 
-router.route('/')
-    .get(stormpath.loginRequired, function(req, res) {
-        c.connect({
-            host: '127.0.0.1',
-            user: 'bsb-api-admin',
-            password: 'BSB0$$dbUser!'
-        });
-        c.on('connect', function() {
-            console.log('Client Connected');
-        });
-        var user = res.locals.user;
+
+router.route('/:user')
+    .get(function(req, res) {
+        var user = req.params('user');
         var campaigns = false;
         var routes = false;
-        var userDataRef = new Firebase('https://vivid-fire-567.firebaseio.com/BSB/userStore/' + user.username);
-        userDataRef.once("value", function(snapshot) {
-            var acctData = snapshot.val();
-            if (acctData !== null) {
-                campaigns = acctData.campaigns;
-                routes = acctData.routes;
-            }
             res.json({
-                title: user.username + "'s Dashboard",
-                user: user.username,
+                user: user,
                 campaigns: campaigns,
                 routes: routes
             });
-        });
     });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
 // call the packages we need
 var path = require('path');
 var logger = require('morgan');
 var multer = require('multer');
-var express    = require('express'); 		// call express
-var app        = express(); 				// define our app using express
+var express = require('express'); 
+var app = express(); 
 var bodyParser = require('body-parser');
 var stormpath = require('express-stormpath');
+var Sequelize = require('sequelize');
 
+// call in other module exports
 var uploads = require('./routes/uploads');
 var upload = require('./routes/upload');
 var index = require('./routes/index');
@@ -24,15 +21,17 @@ var newCampaign = require('./routes/newCampaign');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(multer({
-	dest: './public/images/',
-    rename: function(fieldname, filename){
+    dest: './public/images/',
+    rename: function(fieldname, filename) {
         return fieldname + Date.now();
     }
 }));
@@ -40,35 +39,45 @@ app.use(multer({
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var port = process.env.PORT || 8080; 		// set our port
+var port = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router(); 				// get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
+var sequelize = new Sequelize('bsb-api', 'bsb-api-admin', 'BSB0$$dbUser!', {
+    dialect: mariadb
+});
+var Account = sequelize.import(__dirname + "/models/account.js");
+var Route = sequelize.import(__dirname + "/models/route.js");
+var Campaign = sequelize.import(__dirname + "/models/campaign.js");
+sequelize.sync();
 
 app.use(stormpath.init(app, {
     apiKeyFile: '/Users/jpribesh/.stormpath/apiKey.properties',
     application: 'https://api.stormpath.com/v1/applications/5OEXbG5BaE5SJQ5bGKHKeD',
     secretKey: '2gYxQyynHmlwPsghFwqDjdff3hIUgh778onj',
-	enableUsername: true,
-	requireUsername: true,
-	sessionDuration: 1000 * 60 * 20,
-	cache: 'memory'
+    enableUsername: true,
+    requireUsername: true,
+    sessionDuration: 1000 * 60 * 20,
+    cache: 'memory'
 }));
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 router.use('/dashboard', dashboard);
-app.use('/upload', uploads);
-app.use('/uploaded', upload);
-app.use('/newCampaign', newCampaign);
-app.use('/campaign', campaign);
-app.use('/route', route);
+router.use('/upload', uploads);
+router.use('/uploaded', upload);
+router.use('/newCampaign', newCampaign);
+router.use('/campaign', campaign);
+router.use('/route', route);
 
 
 // error handlers
@@ -100,11 +109,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
 module.exports = app;
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-
-
