056b1bc163161f0e1ea9af523e37b02b33a78573
John Pribesh <beopensourcellc@gmail.com>
2014-12-08 20:27:26 -0500

    added missing comma in campaign model

--- models/campaign.js
+++ models/campaign.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.INTEGER,
             references: "Route",
             referencesKey: "id"
-        }
+        },
         points: DataTypes.BLOB
     }, {
         classMethods: {
