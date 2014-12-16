25e0bf4286edc20cb0104e23632959dba09f1b60
John Pribesh <beopensourcellc@gmail.com>
2014-12-15 22:11:46 -0500

    added new upload.js route

--- routes/upload.js
+++ routes/upload.js
@@ -1,35 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var image = require('../custom_modules/image');
+var orm = require('../lib/model');
+var Campaign = orm.model('Campaign');
+var Photo = orm.model('Photo');
 
 function saveImageInfo(req, res, next) {
-    var user = res.locals.user;
     var count = 0;
     var imageCount = req.finalImages.length;
     Campaign.find({
         where: {
             user: userId
         }
-    }).success(function(campaign) {
+    }).complete(function(err, campaign) {
         campaign.increment('photo_count', imageCount)
-    }).success();
+        var finalImages = req.finalImages;
+        finalImages.forEach(function(image) {
+            Photo.create({
+                caption: image.caption,
+                latitude: image.lat,
+                longitude: image.lng,
+                full: image.full,
+                thumb: image.thumb,
+                photo_date: image.date,
+                upload_date: Date.now()
+            }).complete(function(err, photo) {
+                photo.setCampaign(campaign).complete(function(err) {
+                    campaign.addPhoto(photo).complete(function(err) {
+                        count++;
+                        if (imageCount === count) {
+                            next();
+                        }
+                    });
+                });
+            });
 
-    var campaignPhotosRef = new Firebase('https://vivid-fire-567.firebaseio.com/BSB/userStore/' + user.username + '/campaigns/' + req.campaignId + '/photos');
-    var finalImages = req.finalImages;
-    finalImages.forEach(function(image) {
-        campaignPhotosRef.push(image, function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Data saved successfully for : ' + image.file);
-                count++;
-                if (req.finalImages.length === count) {
-                    next();
-                }
-            }
         });
     });
-
 }
 
 router.route("/:campaignId")
@@ -39,12 +46,19 @@ router.route("/:campaignId")
             where: {
                 id: campaignId
             }
-        }).success(function(campaign) {
-            res.json({
-                title: "File(s) Uploaded Successfully!",
-                campaign: campaign,
-                view: true,
-                scripts: ['https://maps.googleapis.com/maps/api/js?key=AIzaSyCU42Wpv6BtNO51t7xGJYnatuPqgwnwk7c', '/javascripts/getPoints.js']
+        }).complete(function(err, campaign) {
+            campaign.getAccount().complete(function(err, _account) {
+                campaign.getPhotos().complete(function(err, _photos) {
+                    res.json({
+                        campaign: {
+                            info: campaign,
+                            photos: _photos.values
+                        },
+                        account: _account.values,
+                        view: true,
+                        scripts: ['https://maps.googleapis.com/maps/api/js?key=AIzaSyCU42Wpv6BtNO51t7xGJYnatuPqgwnwk7c', '/javascripts/getPoints.js']
+                    });
+                });
             });
         });
     });
